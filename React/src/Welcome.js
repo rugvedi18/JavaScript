32992b0c0bca1b9a077ebfe6ded511428e7b379a
Rugvedi <66817023+rugvedi18@users.noreply.github.com>
2022-02-09 00:15:21 +0530

    Add files via upload

---
+++ React/src/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Welcome = (props) => {
+    return (
+        <div>
+    <h1 style={{color:"red"}}>Welcome {props.name}</h1>
+    </div>
+    )
+}
+
+export default Welcome;
